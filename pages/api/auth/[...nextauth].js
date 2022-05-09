b44c71a69eed7f187ba1f09225368a77c921a2de
JulianWagner <juliantosun@gmail.com>
2022-05-09 13:38:46 +0200

    authorization string

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,11 +12,7 @@ export default NextAuth({
         FacebookProvider({
             clientId      : process.env.FACEBOOK_CLIENT_ID,
             clientSecret  : process.env.FACEBOOK_CLIENT_SECRET,
-            userinfo: {
-                params: {
-                    fields: "email,ads_read,ads_management,public_profile,business_management, read_insights"
-                }
-            }
+            authorization : "https://www.facebook.com/v11.0/dialog/oauth?scope=email,ads_read,ads_management,public_profile,business_management,read_insights"
         }),
         //ToDo: Remove EmailProvider
         EmailProvider({
