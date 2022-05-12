7bfa73878ba969e362ae1bb7ea6f1591e6e52181
JulianWagner <juliantosun@gmail.com>
2022-05-12 12:05:26 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,7 @@
-import { useRouter } from 'next/router'
-import { useSession } from 'next-auth/react'
+
 import Home from '../pages/facebook/adsOverview'
 
 export default function Index({ children }) {
-    const router = useRouter()
-
-    const { status } = useSession({
-        required : true,
-        onUnauthenticated() {
-            router.push('/api/auth/signin')
-        },
-        onAuthenticated() {
-            router.push('/facebook/AdsOverview')
-        }
-    })
 
     return <Home />
 }
